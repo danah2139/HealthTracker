e99e7744a4df7e52127198da8bcb65aa0a3defaa
talya shaltiel <tatalya97@gmail.com>
2019-09-13 11:12:18 +0300

    try to fix the profile

--- BE/User.cs
+++ BE/User.cs
@@ -16,16 +16,25 @@ namespace BE
         public int Height { get; set; }
         public double Weight { get; set; }
         public string DateOfWeight { get; set;}
+<<<<<<< HEAD
         // public DateTime DateOfBirth { get; set; }
+=======
+        public DateTime DateOfBirth { get; set; }
+>>>>>>> d87533e83289c24f980b434e11f395558c41b824
         public enum Gender { male, female,itsComplicated };
         public enum Mood {happy,sad,angry,bored,amused,moody,exited,nervous,stressed };
         public enum FamilyStatus { single,married,divorced,itsComplicated };
         public enum Activity {work,home,fun,study,other };
+<<<<<<< HEAD
         private Gender _gender;
         private Mood _mood;
         private FamilyStatus _familyStatus;
         private Activity _activity;
         public DateTime? DateOfBirth { get; set; }
+=======
+        public List<KeyValuePair<string, double>> lstweights { get; set; }
+        
+>>>>>>> d87533e83289c24f980b434e11f395558c41b824
 
         public List<KeyValuePair<string, double>> lstweights { get; set; }
 
