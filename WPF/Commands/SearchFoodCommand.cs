381718c3ef3312a9b1c454a134fd386f5b869c68
danah2139 <danah2139@gmail.com>
2020-01-07 23:09:47 +0200

    add arrays for food search in enter meals

--- WPF/Commands/SearchFoodCommand.cs
+++ WPF/Commands/SearchFoodCommand.cs
@@ -8,9 +8,17 @@ using WPF.ViewModels;
 
 namespace WPF.Commands
 {
-    class SearchFoodCommand : ICommand
+    public class SearchFoodCommand : ICommand
     {
         public event EventHandler CanExecuteChanged;
+        public EnterMealsVM EnterMealsVM { get; set; }
+        public SearchFoodCommand(EnterMealsVM EnterMealsVM)
+        {
+            this.EnterMealsVM = EnterMealsVM;
+
+        }
+
+
 
         public bool CanExecute(object parameter)
         {
@@ -22,39 +30,39 @@ namespace WPF.Commands
             switch (int.Parse(parameter.ToString()))
             {
                 case 1:
-                    ((EnterMealsVM)parameter).SearchFood(1);
+                    EnterMealsVM.SearchFood(1);
                     break;
                     
                 case 2:
-                    ((EnterMealsVM)parameter).SearchFood(2);
+                    EnterMealsVM.SearchFood(2);
                     break;
 
                 case 3:
-                    ((EnterMealsVM)parameter).SearchFood(3);
+                    EnterMealsVM.SearchFood(3);
                     break;
 
                 case 4:
-                    ((EnterMealsVM)parameter).SearchFood(4);
+                    EnterMealsVM.SearchFood(4);
                     break;
 
                 case 5:
-                    ((EnterMealsVM)parameter).SearchFood(5);
+                    EnterMealsVM.SearchFood(5);
                     break;
 
                 case 6:
-                    ((EnterMealsVM)parameter).SearchFood(6);
+                    EnterMealsVM.SearchFood(6);
                     break;
 
                 case 7:
-                    ((EnterMealsVM)parameter).SearchFood(7);
+                    EnterMealsVM.SearchFood(7);
                     break;
 
                 case 8:
-                    ((EnterMealsVM)parameter).SearchFood(8);
+                    EnterMealsVM.SearchFood(8);
                     break;
 
                 case 9:
-                    ((EnterMealsVM)parameter).SearchFood(9);
+                    EnterMealsVM.SearchFood(9);
                     break;
 
             }
--- WPF/ViewModels/EnterMealsVM.cs
+++ WPF/ViewModels/EnterMealsVM.cs
@@ -9,26 +9,38 @@ using BL;
 using System.Collections.ObjectModel;
 using WPF.UserControls;
 using BE;
+using WPF.Commands;
 
 namespace WPF.ViewModels
 {
     public class EnterMealsVM
 
     {
-       string Name;
-       EnterMealsModel EnterMealsModel { get; set; }
-       EnterMeals EnterMeals { get; set; }
-       List<int>Fats { get; set; }
+       public EnterMealsModel EnterMealsModel { get; set; }
+       public SearchFoodCommand SearchFoodCommand { get; set; }
+       public EnterMeals EnterMeals { get; set; }
+       public double [] Fat { get; set; }
+       public double [] Protein { get; set; }
+       public double [] Carbon { get; set; }
+       public string[] Name { get; set; }
        public EnterMealsVM()
        {
             EnterMealsModel = new EnterMealsModel();
-       }
+            SearchFoodCommand = new SearchFoodCommand(this);
+            Name = new string[10];
+            Fat = new double[10];
+            Carbon = new double[10];
+            Protein = new double[10];
+
+        }
 
         public void SearchFood(int index)
         {
-
-            Name = bindIndexToName(index); 
-            ReportObject.RootObject rootObject = EnterMealsModel.getReportInformation(Name);
+            String name = bindIndexToName(index); 
+            ReportObject.RootObject rootObject = EnterMealsModel.getReportInformation(name);
+            Fat[index] = rootObject.foods[0].food.desc.ff;
+            Carbon[index] = rootObject.foods[0].food.desc.cf;
+            Protein[index] = rootObject.foods[0].food.desc.pf;
 
         }
         public string bindIndexToName(int index)
@@ -36,90 +48,27 @@ namespace WPF.ViewModels
             switch (index)
             {
                 case 1:
-                    return Name1;
+                    return Name[1];
                 case 2:
-                    return Name2;
+                    return Name[2];
                 case 3:
-                    return Name3;
+                    return Name[3];
                 case 4:
-                    return Name4;
+                    return Name[4];
                 case 5:
-                    return Name5;
+                    return Name[5];
                 case 6:
-                    return Name6;
+                    return Name[6];
                 case 7:
-                    return Name7;
+                    return Name[7];
                 case 8:
-                    return Name8;
+                    return Name[8];
                 case 9:
-                    return Name9;
+                    return Name[9];
             }
             return null;
         }
 
-        private string name1;
-        public string Name1
-        {
-            get { return name1; }
-            set { name1 = value; }
-        }
-
-
-        private string name2;
-        public string Name2
-        {
-            get { return name2; }
-            set { name2 = value; }
-        }
-
-        private string name3;
-        public string Name3
-        {
-            get { return name3; }
-            set { name3 = value; }
-        }
-
-        private string name4;
-        public string Name4
-        {
-            get { return name4; }
-            set { name4 = value; }
-        }
-
-        private string name5;
-        public string Name5
-        {
-            get { return name5; }
-            set { name5 = value; }
-        }
-
-        private string name6;
-        public string Name6
-        {
-            get { return name6; }
-            set { name6 = value; }
-        }
-
-        private string name7;
-        public string Name7
-        {
-            get { return name7; }
-            set { name7 = value; }
-        }
-
-        private string name8;
-        public string Name8
-        {
-            get { return name8; }
-            set { name8 = value; }
-        }
-
-        private string name9;
-        public string Name9
-        {
-            get { return name9; }
-            set { name9 = value; }
-        }
 
     }
 }
